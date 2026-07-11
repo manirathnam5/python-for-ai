1571f7f06cb9a226c3c558819b533daee4febf3b
manirathnam5 <manirathnam501@gmail.com>
2026-07-11 23:04:29 +0530

    experimentaion

--- hello.py
+++ hello.py
@@ -57,3 +57,86 @@ if isPremium_user:
     print("User is a premium user.")
 else:
     print("User is not a premium user.")
+
+age = 23
+print(type(age))
+
+
+company = "OpenAI"
+print(type(company))
+
+
+is_active = True
+print(type(is_active))
+
+
+total_amount = 100.50 + 200.75
+print(total_amount)
+
+
+first_name = "Swamy"
+last_name = "Reddy"
+full_name = first_name + " " + last_name
+print(full_name)
+
+
+tab_name = "Samsung"
+print(f"Tab name: {tab_name}")
+
+print(type(4.34))
+
+print(type("Hello"))
+
+print(type(True))
+
+
+pi = 3.14159
+price = 19.99
+temperature = 25.5
+print(f"Pi: {pi}, Price: {price}, Temperature: {temperature}")
+
+total = 100 + 200.5
+print(f"Total: {total}")
+
+cube = 3**3
+print(f"Cube of 3: {cube}")
+
+sqaure = 5**2
+print(f"Square of 5: {sqaure}")
+
+result = 10 / 3
+print(f"Result of 10 divided by 3: {result}")
+
+
+result2 = 10 // 3
+print(f"Result of 10 floor divided by 3: {result2}")
+
+
+income = 50000
+tax = 300
+net_income = income // tax
+print(f"Net income: {net_income}")
+
+
+million = 1_000_000
+print(f"Million: {million}")
+
+welcome_message = "Welcome to the Python world!"
+print(welcome_message)
+
+flat_info = """
+flat number: 504
+area: Marathahalli
+city: Bangalore 
+"""
+print(flat_info)
+
+
+stars = "*" * 5
+print(stars)
+
+name = "John"
+print(len(name))
+
+name = "Alice"
+print(len(name))
